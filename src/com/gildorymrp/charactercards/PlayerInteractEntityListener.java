8804ac2f7e627acf7ca1103d5c56ac135d200ffa
Hohahihehu <oaieuy@hotmail.com>
2014-03-08 23:26:47 -0500

    MySQL Update
    For some reason, this didn't go with the rest?

--- src/com/gildorymrp/charactercards/PlayerInteractEntityListener.java
+++ src/com/gildorymrp/charactercards/PlayerInteractEntityListener.java
@@ -1,44 +1,44 @@
 package com.gildorymrp.charactercards;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
-import com.gildorymrp.gildorymclasses.CharacterClass;
-import com.gildorymrp.gildorymclasses.GildorymClasses;
+import com.gildorymrp.gildorym.Gildorym;
+import com.gildorymrp.gildorym.GildorymCharacter;	
 
 public class PlayerInteractEntityListener implements Listener {
-	
-	private GildorymCharacterCards plugin;
-	
-	public PlayerInteractEntityListener(GildorymCharacterCards plugin) {
-		this.plugin = plugin;
-	}
 	
 	@EventHandler
 	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
 		if (event.getPlayer().isSneaking()) {
 			if (event.getRightClicked() instanceof Player) {
 				Player player = (Player) event.getRightClicked();
-				GildorymClasses gildorymClasses = (GildorymClasses) plugin.getServer().getPluginManager().getPlugin("GildorymClasses");
-				if (plugin.getCharacterCards().get(player.getName()) == null) {
-					plugin.getCharacterCards().put(player.getName(), new CharacterCard(0, Gender.UNKNOWN, "", Race.UNKNOWN, gildorymClasses.levels.get(player.getName()), gildorymClasses.classes.get(player.getName())));
-				}
-				event.getPlayer().sendMessage(ChatColor.BLUE + "" + ChatColor.BOLD + player.getDisplayName() + ChatColor.BLUE + ChatColor.BOLD + "'s character card");
-				CharacterCard characterCard = plugin.getCharacterCards().get(player.getName());
-				CharacterClass clazz = gildorymClasses.classes.get(player.getName());
-				Integer level = gildorymClasses.levels.get(player.getName());
-				Race race = characterCard.getRace();
-
-				Integer maxHealth = CharacterCard.calculateHealth(clazz, race, level);
+				Gildorym gildorym = (Gildorym) Bukkit.getServer().getPluginManager().getPlugin("Gildorym");
+				GildorymCharacter gChar = gildorym.getActiveCharacters().get(player.getName());
+				CharacterCard characterCard = gChar.getCharCard();
+				Integer maxHealth = CharacterCard.calculateHealth(gChar);
 				
+				event.getPlayer().sendMessage(ChatColor.BLUE + "" + ChatColor.BOLD + player.getDisplayName() + ChatColor.BLUE + ChatColor.BOLD + "'s character card");
 				event.getPlayer().sendMessage(ChatColor.GRAY + "Health: " + ChatColor.BLUE + characterCard.getHealth() + "/" + maxHealth);
 				event.getPlayer().sendMessage(ChatColor.GRAY + "Age: " + ChatColor.WHITE + characterCard.getAge());
 				event.getPlayer().sendMessage(ChatColor.GRAY + "Gender: " + ChatColor.WHITE + characterCard.getGender().toString());
 				event.getPlayer().sendMessage(ChatColor.GRAY + "Race: " + ChatColor.WHITE + characterCard.getRace().toString());
 				event.getPlayer().sendMessage(ChatColor.GRAY + "Description: " + ChatColor.WHITE + characterCard.getDescription());
+				if (event.getPlayer().hasPermission("gildorym.viewalignment")) {
+					String alignmentMessage = ChatColor.GRAY + "Alignment: " + ChatColor.WHITE;
+					if (characterCard.getBehavior() == CharacterBehavior.UNKNOWN || characterCard.getMorality() == CharacterMorality.UNKNOWN) {
+						alignmentMessage += "UNKNOWN";
+					} else if (characterCard.getBehavior() == CharacterBehavior.NEUTRAL && characterCard.getMorality() == CharacterMorality.NEUTRAL) {
+						alignmentMessage += "TRUE NEUTRAL";
+					} else {
+						alignmentMessage += characterCard.getBehavior() + " " + characterCard.getMorality();
+					}
+					event.getPlayer().sendMessage(alignmentMessage);
+				}
 			}
 		}
 	}
