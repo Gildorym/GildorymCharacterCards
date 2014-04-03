47613b4d7ebafa2955ccf3291d98be55835d18c5
Timothy Moore <tjstretchalot@gmail.com>
2014-04-03 13:30:32 -0700

    Update races
    Include all the races so that you can at least set them, although
    there is no difference codewise atm.

--- src/com/gildorymrp/charactercards/Subrace.java
+++ src/com/gildorymrp/charactercards/Subrace.java
@@ -2,6 +2,26 @@ package com.gildorymrp.charactercards;
 
 public enum Subrace {
 	DROW(Race.ELF, true),
+	MOON_ELF(Race.ELF, false),
+	SUN_ELF(Race.ELF, false),
+	WOOD_ELF(Race.ELF, false),
+	DARK_ELF(Race.ELF, false),
+	DAMARAN(Race.HUMAN, true),
+	ILLUSKAN(Race.HUMAN, false),
+	MULHORANDHI(Race.HUMAN, false),
+	CALISHITE(Race.HUMAN, false),
+	CHONDATHANS(Race.HUMAN, false),
+	ROCK_GNOME(Race.GNOME, true),
+	FOREST_GNOME(Race.GNOME, false),
+	DEEP_GNOME(Race.GNOME, false),
+	LIGHTFOOT_HALFLING(Race.HALFLING, true),
+	STRONGHEART_HALFLING(Race.HALFLING, false),
+	HILL_DWARF(Race.DWARF, true),
+	SHIELD_DWARf(Race.DWARF, false),
+	GREY_DWARF(Race.DWARF, false),
+	HALF_ELF(Race.HALFELF, true),
+	HALF_ORC(Race.HALFORC, true),
+	
 	
 	UNKNOWN(Race.UNKNOWN, true);
 	
